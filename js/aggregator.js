4b813d1abf596aa73f93cf5f89dae48e81fe1cf0
as <andrzej.salak@gmail.com>
2020-02-09 20:58:30 +0100

    Code comments

--- js/aggregator.js
+++ js/aggregator.js
@@ -1,14 +1,48 @@
-process.stdin.on('data', processLine)
+/**
+ * Usage:
+ * cat csv/round18-cloud-* | node js/aggregator.js > csv/round18-aggregated-cloud.csv
+ * cat csv/round18-physical-* | node js/aggregator.js > csv/round18-aggregated-physical.csv
+ * cat csv/round18-cloud-* csv/round18-physical-* | node js/aggregator.js > csv/round18-aggregated.csv
+ */
 
 const languagesMap = {};
-let resultArray = []
+let resultArray = [];
+
+process.stdin.on('data', processLine);
+
+
+/**
+ * Creates languagesMap which looks like
+ * {
+ *     C: [ 100, 70.5, 88 ...],
+ *     Rus: [ 75.5, 83.8, 63.9 ...],
+ *     vb: [ 50.3, 62, 70.2 ...],
+ *     [...]
+ * }
+ * @param {String} lines
+ */
+function processLine(lines) {
+    ("" + lines).split("\n").forEach(line => {
+        const splitted = line.split(",")
+        const lang = splitted[0]
+        const result = Number.parseFloat(splitted[1])
+
+        if (result && lang) {
+            if (!languagesMap[lang]) {
+                languagesMap[lang] = [];
+            }
+            languagesMap[lang].push(result);
+        }
+    });
+    // console.log(languagesMap)
+    calcAverage();
+}
 
 function calcAverage() {
     for (let [key, value] of Object.entries(languagesMap)) {
-        resultArray.push([key, average(value)])
+        resultArray.push([key, average(value)]);
     }
-    // console.log(resultArray)
-    sortResultsArray()
+    sortResultsArray();
 }
 
 function sortResultsArray() {
@@ -18,35 +52,18 @@ function sortResultsArray() {
         return 0;
     });
 
-    normalizeTo100Percent()
+    normalizeTo100Percent();
 }
 
 function normalizeTo100Percent() {
     resultArray = resultArray.map(row => {
-        return [row[0], parseFloat(Number(row[1] * 100 / resultArray[0][1]).toFixed(1))]
+        return [row[0], parseFloat(Number(row[1] * 100 / resultArray[0][1]).toFixed(1))];
     })
-    console.log(resultArray.map(v => v.join(",")).join("\n"))
+    // this is final result for script run
+    console.log(resultArray.map(v => v.join(",")).join("\n"));
 }
 
+/* HELPERS */
 function average(arr) {
     return arr.reduce((p, c) => p + c, 0) / arr.length;
-}
-
-function processLine(lines) {
-    ("" + lines).split("\n").forEach(line => {
-        const splitted = line.split(",")
-        const lang = splitted[0]
-        const result = Number.parseFloat(splitted[1])
-
-        if (result && lang ) {
-
-            if (!languagesMap[lang]) {
-                languagesMap[lang] = []
-            }
-            languagesMap[lang].push(result)
-        }
-    });
-    // console.log(languagesMap)
-
-    calcAverage();
 }
